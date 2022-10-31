22b314e0fe5624ebe4e8c4866d3b1edb9dcbb19e
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2022-10-31 12:43:11 +0100

    life is life

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,49 @@ package org.example;
 
 import io.javalin.Javalin;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         System.out.println("Hello world!");
 
         var app = Javalin.create().get("/", ctx -> ctx.result("Hello World")).start();
         app.get("/",context -> {
             context.result("Hello World");
         });
+    }
+
+    */
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int L = 500;
+        int H = 100;
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+        String T = in.nextLine();
+
+        BufferedImage image = new BufferedImage(L, H, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        g.setFont(new Font("SansSerif", Font.BOLD, 24));
+
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        graphics.drawString(T, 10, 20);
+
+        for (int y = 0; y < H; y++) {
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < L; x++) {
+                sb.append(image.getRGB(x, y) == -16777216 ? " " : "#");
+            }
+            if (sb.toString().trim().isEmpty()) {
+                continue;
+            }
+            System.out.println(sb);
+        }
     }
 }
