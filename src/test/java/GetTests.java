40f4b6c87269c8d42753816d0a9450d05bdc7ee5
denkaforss <dennis.forss@gmail.com>
2022-10-31 14:11:15 +0100

    second test commit (get questions)

--- src/test/java/GetTests.java
+++ src/test/java/GetTests.java
@@ -1,6 +1,7 @@
 import org.example.InMemory;
 import org.example.QuestionRepo;
 import org.example.Questions;
+import org.example.exceptions.DoesNotExistException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -23,5 +24,15 @@ public class GetTests {
         repo.add(question);
 
         Assertions.assertEquals(question, repo.getQuestion(question.getId()));
+    }
+
+    @Test
+    @DisplayName("Get specific question don't exist")
+    void getSpecificQuestionDontExist(){
+        Questions question = new Questions(1, "What is life?", new String[] {"Coffee", "Coding", "Pizza"}, "Studiegrupp 7" );
+
+        repo.add(question);
+
+        Assertions.assertThrows(DoesNotExistException.class, () -> repo.getQuestion(45));
     }
 }
