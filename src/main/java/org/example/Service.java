3e2ee0a03940de834385feebe50b849d311f513d
denkaforss <dennis.forss@gmail.com>
2022-11-01 09:44:35 +0100

    second service implementation commit (add question)

--- src/main/java/org/example/Service.java
+++ src/main/java/org/example/Service.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.exceptions.AlreadyExistsException;
+import org.example.exceptions.DoesNotExistException;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class Service  {
 
@@ -11,7 +15,11 @@ public class Service  {
         this.repo = repo;
     }
 
-    public Questions addQuestion(int id, String question, String[] answers, String correctAnswer) {
+    public Questions addQuestion(int id, String question, String[] answers, String correctAnswer) throws DoesNotExistException, AlreadyExistsException {
+        Optional<Questions> existing = Optional.ofNullable(repo.getQuestion(id));
+        if (existing.isPresent()) {
+            throw new AlreadyExistsException();
+        }
         Questions newQuestion = new Questions(id, question, answers, correctAnswer);
         repo.add(newQuestion);
         return newQuestion;
