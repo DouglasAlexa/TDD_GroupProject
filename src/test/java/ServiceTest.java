a3913977a3b6e5edf7b76d2b654665d9d7c17962
denkaforss <dennis.forss@gmail.com>
2022-11-01 09:40:37 +0100

    second service test commit (add question) also added AlreadyExistsException.java

--- src/test/java/ServiceTest.java
+++ src/test/java/ServiceTest.java
@@ -1,11 +1,15 @@
 import org.example.QuestionRepo;
 import org.example.Questions;
 import org.example.Service;
+import org.example.exceptions.AlreadyExistsException;
+import org.example.exceptions.DoesNotExistException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.util.Optional;
 
 public class ServiceTest {
 
@@ -24,5 +28,14 @@ public class ServiceTest {
         Questions questions = Assertions.assertDoesNotThrow(() -> service.addQuestion(1, "vad heter jag", new String[]{"David", "Dennis", "Douglas"}, "Konstantin"));
         Mockito.verify(repo).add(Mockito.any(Questions.class));
         Assertions.assertNotNull(questions);
+    }
+
+    @Test
+    @DisplayName("Add question no duplicates")
+    void addQuestionUsingServiceClassNoDuplicates() throws DoesNotExistException {
+        Questions question = new Questions(1, "vad heter jag", new String[]{"David", "Dennis", "Douglas"}, "Konstantin");
+        Mockito.when(repo.getQuestion(1)).thenReturn(question);
+        Assertions.assertThrows(AlreadyExistsException.class, () -> service.addQuestion(1, "vad heter jag", new String[]{"David", "Dennis", "Douglas"}, "Konstantin"));
+        Mockito.verify(repo, Mockito.never()).add(Mockito.any(Questions.class));
     }
 }
