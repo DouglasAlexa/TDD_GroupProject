1a92ef60e06e41b896aa8e42217784e84b9a9038
denkaforss <dennis.forss@gmail.com>
2022-11-02 14:37:59 +0100

    second controller implementation commit (add question)

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -15,16 +15,22 @@ public class Controller {
     public Controller(Service service) {
         this.service = service;
     }
+
     public void add(Context context) throws DoesNotExistException, AlreadyExistsException {
         Questions questions = gson.fromJson(context.body(), Questions.class);
-        Integer id = questions.getId();
-        String question = questions.getQuestion();
-        String[] answers = questions.getAnswer();
-        String correctAnswer = questions.getCorrectAnswer();
-        Questions createdQuestion = service.addQuestion(id, question, answers, correctAnswer);
-        String jsonString = gson.toJson(createdQuestion);
-        context.status(HttpStatus.CREATED);
-        context.json(jsonString);
+        if (questions == null) {
+            context.status(HttpStatus.BAD_REQUEST);
+            context.result("Bad Request");
+        } else {
+            Integer id = questions.getId();
+            String question = questions.getQuestion();
+            String[] answers = questions.getAnswer();
+            String correctAnswer = questions.getCorrectAnswer();
+            Questions createdQuestion = service.addQuestion(id, question, answers, correctAnswer);
+            String jsonString = gson.toJson(createdQuestion);
+            context.status(HttpStatus.CREATED);
+            context.json(jsonString);
+        }
     }
 
     public void delete(Context context) {
